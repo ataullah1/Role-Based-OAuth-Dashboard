59d36519110b981f60af5833a393a8b3182f8822
Md Ataullah <ataullahm100@gmail.com>
2025-02-03 15:53:11 +0600

    Enhance AppSidebar with user profile dropdown and company details; update layout to fetch current user

--- app/(protected)/_components/app-sidebar.tsx
+++ app/(protected)/_components/app-sidebar.tsx
@@ -24,9 +24,37 @@ import {
   CollapsibleContent,
   CollapsibleTrigger,
 } from "@/components/ui/collapsible";
-import { ChevronRight } from "lucide-react";
+import {
+  Bell,
+  ChevronRight,
+  ChevronsUpDown,
+  CircleUserRound,
+  GalleryVerticalEnd,
+} from "lucide-react";
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuGroup,
+  DropdownMenuItem,
+  DropdownMenuLabel,
+  DropdownMenuSeparator,
+  DropdownMenuTrigger,
+} from "@/components/ui/dropdown-menu";
+import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
 
-export function AppSidebar() {
+export const company = {
+  name: "Hello Company",
+  logo: GalleryVerticalEnd,
+  plan: "Administrator",
+};
+
+interface User {
+  image?: string;
+  name?: string;
+  email?: string;
+}
+
+export function AppSidebar({ user }: { user: User }) {
   const pathname = usePathname();
 
   return (
@@ -34,11 +62,11 @@ export function AppSidebar() {
       <SidebarHeader>
         <div className="flex gap-2 py-2 text-sidebar-accent-foreground">
           <div className="flex aspect-square size-8 items-center justify-center rounded-lg bg-sidebar-primary text-sidebar-primary-foreground">
-            {/* < className="size-4" /> */}
+            <company.logo className="size-4" />
           </div>
           <div className="grid flex-1 text-left text-sm leading-tight">
-            <span className="truncate font-semibold">Company Name</span>
-            <span className="truncate text-xs">Plan Name</span>
+            <span className="truncate font-semibold">{company.name}</span>
+            <span className="truncate text-xs">{company.plan}</span>
           </div>
         </div>
       </SidebarHeader>
@@ -105,9 +133,91 @@ export function AppSidebar() {
       </SidebarContent>
 
       <SidebarFooter>
-        <div className="absolute bottom-4 w-full">
-          <UserButton />
-        </div>
+        <SidebarMenu>
+          <SidebarMenuItem>
+            <DropdownMenu>
+              <DropdownMenuTrigger asChild>
+                <SidebarMenuButton
+                  size="lg"
+                  className="data-[state=open]:bg-sidebar-accent data-[state=open]:text-sidebar-accent-foreground"
+                >
+                  <Avatar className="h-8 w-8 rounded-lg">
+                    <AvatarImage
+                      src={user?.image as string}
+                      alt={user?.name as string}
+                    />
+                    <AvatarFallback className="rounded-lg">
+                      {user?.name?.slice(0, 2)?.toUpperCase() || "CN"}
+                    </AvatarFallback>
+                  </Avatar>
+                  <div className="grid flex-1 text-left text-sm leading-tight">
+                    <span className="truncate font-semibold">
+                      {user?.name || ""}
+                    </span>
+                    <span className="truncate text-xs">
+                      {user?.email || ""}
+                    </span>
+                  </div>
+                  <ChevronsUpDown className="ml-auto size-4" />
+                </SidebarMenuButton>
+              </DropdownMenuTrigger>
+              <DropdownMenuContent
+                className="w-[--radix-dropdown-menu-trigger-width] min-w-56 rounded-lg"
+                side="bottom"
+                align="end"
+                sideOffset={4}
+              >
+                <DropdownMenuLabel className="p-0 font-normal">
+                  <div className="flex items-center gap-2 px-1 py-1.5 text-left text-sm">
+                    <Avatar className="h-8 w-8 rounded-lg">
+                      <AvatarImage
+                        src={user?.image || ""}
+                        alt={user?.name || ""}
+                      />
+                      <AvatarFallback className="rounded-lg font-bold">
+                        {user?.name?.slice(0, 2)?.toUpperCase() || "CN"}
+                      </AvatarFallback>
+                    </Avatar>
+                    <div className="grid flex-1 text-left text-sm leading-tight">
+                      <span className="truncate font-semibold">
+                        {user?.name || ""}
+                      </span>
+                      <span className="truncate text-xs">
+                        {" "}
+                        {user?.email || ""}
+                      </span>
+                    </div>
+                  </div>
+                </DropdownMenuLabel>
+                <DropdownMenuSeparator />
+
+                <DropdownMenuGroup>
+                  <DropdownMenuItem>
+                    <Link href={"/dashboard"} className="flex flex-row gap-2">
+                      <Bell />
+                      Notifications
+                    </Link>
+                  </DropdownMenuItem>
+                  <DropdownMenuItem>
+                    <Link
+                      href={"/dashboard/profile"}
+                      className="flex flex-row gap-2"
+                    >
+                      <CircleUserRound />
+                      Account
+                    </Link>
+                  </DropdownMenuItem>
+                </DropdownMenuGroup>
+                <DropdownMenuSeparator />
+
+                <DropdownMenuItem>
+                  <UserButton />
+                  Log out
+                </DropdownMenuItem>
+              </DropdownMenuContent>
+            </DropdownMenu>
+          </SidebarMenuItem>
+        </SidebarMenu>
       </SidebarFooter>
     </Sidebar>
   );
--- app/(protected)/dashboard/layout.tsx
+++ app/(protected)/dashboard/layout.tsx
@@ -2,17 +2,23 @@ import { SidebarInset, SidebarProvider } from "@/components/ui/sidebar";
 import { AppSidebar } from "../_components/app-sidebar";
 import Header from "../_components/header";
 import KBar from "@/components/kbar";
+import { currentUser } from "@/lib/auth";
+import { redirect } from "next/navigation";
 
 interface ProtectedLayoutProps {
   children: React.ReactNode;
 }
 
-const ProtectedLayout = ({ children }: ProtectedLayoutProps) => {
+const ProtectedLayout = async ({ children }: ProtectedLayoutProps) => {
+  const user = await currentUser();
+  if (!user) {
+    return redirect("/");
+  }
   return (
     <KBar>
       <SidebarProvider defaultOpen={false}>
-        <AppSidebar />
-        <SidebarInset>
+        <AppSidebar user={user} />
+        <SidebarInset className="px-3">
           <Header />
           {/* page main content */}
           {children}
